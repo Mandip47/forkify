8948f74143a321a1e1201101a59a9ef272937027
Mandip Chhetri <108076760+Mandip47@users.noreply.github.com>
2023-11-14 22:42:44 +0545

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '11e4c89a-3670-420b-b23a-7458bbf5ad05';
+export const MODAL_CLOSE_SEC = 2.5;
